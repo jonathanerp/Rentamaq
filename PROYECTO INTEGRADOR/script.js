2de78a774bcdc7f3a2219972bdcce45edcc1bde8
Luciano <luchonicolosi01@gmail.com>
2024-02-28 01:17:32 -0300

    tarea terminada producto por id

--- PROYECTO INTEGRADOR/script.js
+++ PROYECTO INTEGRADOR/script.js
@@ -1,24 +1,105 @@
-document.querySelector('.logo-section a').addEventListener('click', function() {
-    window.location.href = 'index.html';
+const productosConstruccionPesada = [
+  {
+    id: 1,
+    name: 'EXCAVADORA KOMATSU',
+    image: 'imagenes/recomendado1.jpg',
+    description: 'Descripción del producto 1...',
+  },
+  {
+    id: 2,
+    name: 'BULLDOZER CATERPILLAR',
+    image: 'imagenes/recomendado2.jpg',
+    description: 'Descripción del producto 2...',
+  },
+  {
+    id: 3,
+    name: 'RETROEXCAVADORA CATERPILLAR',
+    image: 'imagenes/recomendado3.jpg',
+    description: 'Descripción del producto 3...',
+  },
+  {
+    id: 4,
+    name: 'APLANADORA CATERPILLAR',
+    image: 'imagenes/recomendado4.jpg',
+    description: 'Descripción del producto 4...',
+  },
+  {
+    id: 5,
+    name: 'MINICARGADOR BOBCAT',
+    image: 'imagenes/recomendado5.jpg',
+    description: 'Descripción del producto 5...',
+  },
+  {
+    id: 6,
+    name: 'MONTACARGA MITSUBISHI',
+    image: 'imagenes/recomendado6.jpg',
+    description: 'Descripción del producto 6...',
+  },
+  {
+    id: 7,
+    name: 'ANDAMIOS TUBULARES',
+    image: 'imagenes/recomendado7.jpg',
+    description: 'Descripción del producto 7...',
+  },
+  {
+    id: 8,
+    name: 'MOTONIVELADORA CATERPILLAR',
+    image: 'imagenes/recomendado8.jpg',
+    description: 'Descripción del producto 8...',
+  },
+  {
+    id: 9,
+    name: 'VOLQUETA INTERNATIONAL',
+    image: 'imagenes/recomendado9.jpg',
+    description: 'Descripción del producto 9...',
+  },
+  {
+    id: 10,
+    name: 'EXCAVADORA KOMATSU',
+    image: 'imagenes/recomendado10.jpg',
+    description: 'Descripción del producto 10...',
+  },
+];
+window.addEventListener('load', function () {
+  function renderizarRecomendaciones() {
+    const cardsRecomendaciones = document.querySelector('#recomendaciones');
+    console.log(cardsRecomendaciones);
+    // Limpiar el contenido existente en caso de que se estén mostrando productos antiguos
+    cardsRecomendaciones.innerHTML = '';
+    productosConstruccionPesada.forEach((prod) => {
+      cardsRecomendaciones.innerHTML += `
+              <div class="recoment-card">
+                  <img class="img" src=${prod.image} alt=${prod.name} />
+                  <h3>${prod.name}</h3>
+                  <div class="btn-ver">
+                    <h2>ver mas</h2>
+                    <a href="./detalle_producto.html?id=${prod.id}">
+                        <i class="fa-solid fa-circle-plus"></i>
+                    </a>
+                  </div>
+              </div>
+                    `;
+    });
+  }
+  renderizarRecomendaciones();
+
+  document
+    .querySelector('.logo-section a')
+    .addEventListener('click', function () {
+      window.location.href = 'index.html';
+    });
+
+  // Agrega un evento al formulario de búsqueda
+  document
+    .getElementById('searchForm')
+    .addEventListener('submit', function (event) {
+      event.preventDefault(); // Evita que el formulario se envíe por defecto (puede personalizarse según tus necesidades)
+
+      // Obtén el valor del campo de búsqueda
+      const searchQuery = document.getElementById('searchInput').value;
+
+      // Puedes redirigir a una página de resultados de búsqueda o realizar otras acciones según el valor de searchQuery
+      // Por ejemplo:
+      //window.location.href = 'resultados.html?q=' + encodeURIComponent(searchQuery);
+    });
 });
-
-// Agrega un evento al formulario de búsqueda
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault();// Evita que el formulario se envíe por defecto (puede personalizarse según tus necesidades)
-
-// Obtén el valor del campo de búsqueda
-const searchQuery = document.getElementById('searchInput').value;
-
-
-// Puedes redirigir a una página de resultados de búsqueda o realizar otras acciones según el valor de searchQuery
-    // Por ejemplo:
-//window.location.href = 'resultados.html?q=' + encodeURIComponent(searchQuery);
-
-});
-
-
-
-
-
-
-    
--- PROYECTO INTEGRADOR/script_detalle_producto.js
+++ PROYECTO INTEGRADOR/script_detalle_producto.js
@@ -1,30 +1,117 @@
-// Supongamos que tienes un objeto producto con datos específicos
-const producto = {
+const productosConstruccionPesada = [
+  {
     id: 1,
-    nombre: 'Nombre del Producto',
-    descripcion: 'Texto descriptivo del producto.',
-    imagenes: ['imagen1.jpg', 'imagen2.jpg', 'imagen3.jpg'],
-};
+    carrousel: [],
+    name: 'EXCAVADORA KOMATSU',
+    image: 'imagenes/recomendado1.jpg',
+    description:
+      'La Excavadora Komatsu es una máquina poderosa y versátil diseñada para trabajos de construcción de cualquier tamaño. Equipada con un motor robusto y componentes de alta calidad, esta excavadora puede manejar una variedad de tareas, desde la excavación de zanjas hasta la demolición de edificios. Su diseño ergonómico y controles intuitivos hacen que sea fácil de operar y cómoda de usar durante largas jornadas de trabajo.',
+  },
+  {
+    id: 2,
+    carrousel: [],
+    name: 'BULLDOZER CATERPILLAR',
+    image: 'imagenes/recomendado2.jpg',
+    description:
+      'El Bulldozer Caterpillar es una máquina impresionante diseñada para el trabajo duro en cualquier obra de construcción. Equipado con un motor de alta potencia y un tren de rodaje duradero, este bulldozer puede enfrentarse a terrenos difíciles y cargar y empujar grandes cantidades de tierra y escombros con facilidad. Su cabina espaciosa y cómoda ofrece al operador una excelente visibilidad y un entorno de trabajo seguro.',
+  },
+  {
+    id: 3,
+    carrousel: [],
+    name: 'RETROEXCAVADORA CATERPILLAR',
+    image: 'imagenes/recomendado3.jpg',
+    description:
+      'La Retroexcavadora Caterpillar es una máquina versátil y eficiente diseñada para una variedad de tareas de excavación y carga en la construcción. Equipada con una cabina cómoda y controles intuitivos, esta retroexcavadora ofrece una excelente visibilidad y comodidad para el operador. Con su potente motor y componentes duraderos, esta máquina puede manejar una variedad de terrenos y condiciones de trabajo.',
+  },
+  {
+    id: 4,
+    carrousel: [],
+    name: 'APLANADORA CATERPILLAR',
+    image: 'imagenes/recomendado4.jpg',
+    description:
+      'La Aplanadora Caterpillar es una máquina poderosa y versátil diseñada para niveles precisos y un acabado suave en una variedad de superficies. Equipada con un motor potente y componentes duraderos, esta aplanadora puede manejar terrenos difíciles y proporcionar un rendimiento constante y confiable. Su diseño ergonómico y controles intuitivos hacen que sea fácil de operar y cómoda de usar durante largas jornadas de trabajo.',
+  },
+  {
+    id: 5,
+    carrousel: [],
+    name: 'MINICARGADOR BOBCAT',
+    image: 'imagenes/recomendado5.jpg',
+    description:
+      'El Minicargador Bobcat es una máquina compacta y versátil diseñada para trabajos en espacios reducidos. Equipado con una variedad de implementos y accesorios, este minicargador puede manejar una amplia gama de tareas, desde la carga y descarga de materiales hasta la nivelación de terrenos. Su diseño compacto y maniobrable, combinado con una cabina cómoda y controles intuitivos, lo convierte en una opción ideal para trabajos en áreas urbanas y espacios confinados.',
+  },
+  {
+    id: 6,
+    carrousel: [],
+    name: 'MONTACARGA MITSUBISHI',
+    image: 'imagenes/recomendado6.jpg',
+    description:
+      'El Montacarga Mitsubishi es una máquina robusta y confiable diseñada para el movimiento eficiente de materiales en almacenes y depósitos. Equipado con un motor potente y componentes duraderos, este montacarga puede manejar cargas pesadas y proporcionar un rendimiento constante y confiable. Su diseño ergonómico y controles intuitivos hacen que sea fácil de operar y cómodo de usar durante largas jornadas de trabajo.',
+  },
+  {
+    id: 7,
+    carrousel: [],
+    name: 'ANDAMIOS TUBULARES',
+    image: 'imagenes/recomendado7.jpg',
+    description:
+      'Los Andamios Tubulares son una solución versátil y segura para trabajos de construcción y mantenimiento en altura. Fabricados con materiales de alta calidad y un diseño duradero, estos andamios ofrecen una plataforma estable y segura para los trabajadores. Fáciles de montar y desmontar, estos andamios se adaptan a una variedad de aplicaciones y entornos de trabajo.',
+  },
+  {
+    id: 8,
+    carrousel: [],
+    name: 'MOTONIVELADORA CATERPILLAR',
+    image: 'imagenes/recomendado8.jpg',
+    description:
+      'La Motoniveladora Caterpillar es una máquina versátil y eficiente diseñada para una variedad de tareas de nivelación y acabado en la construcción. Equipada con un potente motor y componentes duraderos, esta motoniveladora puede manejar terrenos difíciles y proporcionar un rendimiento constante y confiable. Su diseño ergonómico y controles intuitivos hacen que sea fácil de operar y cómoda de usar durante largas jornadas de trabajo.',
+  },
+  {
+    id: 9,
+    carrousel: [],
+    name: 'VOLQUETA INTERNATIONAL',
+    image: 'imagenes/recomendado9.jpg',
+    description:
+      'La Volqueta International es una máquina robusta y confiable diseñada para el transporte eficiente de materiales en la construcción. Equipada con un motor potente y componentes duraderos, esta volqueta puede manejar cargas pesadas y proporcionar un rendimiento constante y confiable. Su diseño ergonómico y controles intuitivos hacen que sea fácil de operar y cómodo de usar durante largas jornadas de trabajo.',
+  },
+  {
+    id: 10,
+    carrousel: [],
+    name: 'EXCAVADORA KOMATSU',
+    image: 'imagenes/recomendado10.jpg',
+    description:
+      'La Excavadora Komatsu es una máquina poderosa y versátil diseñada para trabajos de construcción de cualquier tamaño. Equipada con un motor robusto y componentes de alta calidad, esta excavadora puede manejar una variedad de tareas, desde la excavación de zanjas hasta la demolición de edificios. Su diseño ergonómico y controles intuitivos hacen que sea fácil de operar y cómoda de usar durante largas jornadas de trabajo.',
+  },
+];
+window.addEventListener('load', function () {
+  // Función para cargar los datos del producto en la página
+  function cargarDetalleProducto() {
+    const divDetalleProducto = document.querySelector('#detalle-producto');
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    console.log(divDetalleProducto);
+    // Obtener el valor de un parámetro específico
+    const prodId = urlSearchParams.get('id');
 
-// Función para cargar los datos del producto en la página
-function cargarDetalleProducto() {
-    // Actualizar el título del producto
-    const tituloProductoElement = document.getElementById('titulo-producto');
-    tituloProductoElement.textContent = producto.nombre;
-
-    // Actualizar la descripción del producto
-    const descripcionProductoElement = document.getElementById('descripcion-producto');
-    descripcionProductoElement.textContent = producto.descripcion;
-
-    // Crear elementos para la galería de imágenes del producto
-    const galeriaImagenesElement = document.getElementById('galeria-imagenes');
-    producto.imagenes.forEach(imagen => {
-        const imagenElement = document.createElement('img');
-        imagenElement.src = imagen;
-        imagenElement.alt = producto.nombre;
-        galeriaImagenesElement.appendChild(imagenElement);
+    divDetalleProducto.innerHTML = '';
+    productosConstruccionPesada.forEach((prod) => {
+      console.log(prod.id);
+      console.log(prodId);
+      if (prod.id.toString() === prodId) {
+        divDetalleProducto.innerHTML += `
+        <div class="div-imagen">
+            <img src=${prod.image} alt=${prod.name} />
+         </div>
+        <div class="div-texto">
+            <h1>${prod.name}</h1>
+            <div>
+                <h3>Descripción General</h3>
+                <p>
+                ${prod.description}
+                </p>
+            </div>
+        </div>
+            `;
+      }
     });
-}
+  }
 
-// Llamar a la función para cargar los datos del producto al cargar la página
-cargarDetalleProducto();
+  // Llamar a la función para cargar los datos del producto al cargar la página
+  cargarDetalleProducto();
+});
