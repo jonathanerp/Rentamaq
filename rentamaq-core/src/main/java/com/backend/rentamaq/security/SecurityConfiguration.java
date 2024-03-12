e3884cafeee2b382a28c02456e3ba937b3eada14
Luciano <luchonicolosi01@gmail.com>
2024-03-12 14:08:51 -0300

    fix security /productos

--- rentamaq-core/src/main/java/com/backend/rentamaq/security/SecurityConfiguration.java
+++ rentamaq-core/src/main/java/com/backend/rentamaq/security/SecurityConfiguration.java
@@ -38,7 +38,7 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                                 .requestMatchers(
-
+                                        "/productos",
                                         "/productos/**",
                                         "/roles",
                                         "/auth/signup",
